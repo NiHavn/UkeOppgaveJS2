918744b6574e0b229376c3da8b78b19684551f05
Niklas Havnaas <Niklas.o.h@hotmail.com>
2022-01-19 12:19:44 +0100

    Initial commit

---
+++ src/test/java/com/example/ukeoppgavejs2/UkeOppgaveJs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgavejs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkeOppgaveJs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
